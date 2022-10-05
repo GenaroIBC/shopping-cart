86f8ae1b4c80b197db0b8a9b2a19c343f931eb86
genaroibc <bonavitaismael@gmail.com>
2022-10-05 18:20:10 -0300

    add main DB operations

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,8 +3,48 @@ import { ProductModel } from "models/ProductModel";
 export async function getAllProducts() {
   try {
     const products = await ProductModel.find();
-
     return products || [];
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function getOneProductById(product_id) {
+  try {
+    const product = await ProductModel.findById(product_id);
+    return product || {};
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function updateOneProductById(product_id, body) {
+  try {
+    const product = await ProductModel.findByIdAndUpdate(product_id, body, {
+      new: true,
+    });
+    return product || {};
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function deleteOneProductById(product_id) {
+  try {
+    const product = await ProductModel.findByIdAndDelete(product_id);
+    return product || {};
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function createOneProduct(data) {
+  try {
+    const newProduct = new ProductModel(data);
+
+    await newProduct.save();
+
+    return newProduct || {};
   } catch (error) {
     return error;
   }
