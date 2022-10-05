f61ad7cadd77663d893ea3c8037339e33491c25d
genaroibc <bonavitaismael@gmail.com>
2022-10-05 11:36:55 -0300

    add mongoDB connection service

---
+++ src/services/mongoDBConnection.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const URI = "mongodb://localhost:27017/shopping-cart";
+let isConnected = false;
+
+export function mongoDBConnection() {
+  if (!isConnected) {
+    mongoose.connect(URI).then(console.log);
+    isConnected = true;
+  }
+}
